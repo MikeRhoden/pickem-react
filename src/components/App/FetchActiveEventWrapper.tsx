c443d472ab32d0db71f2fc0cbe4059b48c8b91fc
Mike Rhoden <mike.rhoden.dev@gmail.com>
2021-10-13 18:16:43 -0500

    refactored FetchActiveEVentWrapper.js -> FetchActiveEventWrapper.tsx

--- src/components/App/FetchActiveEventWrapper.tsx
+++ src/components/App/FetchActiveEventWrapper.tsx
@@ -1,13 +1,19 @@
 import { useEffect } from 'react'
+import { IEvent } from '../../models/IEvent'
 import { fetchEvent } from '../../services/activeEvent'
 
-export default function FetchActiveEventWrapper({ setEvent }) {
+interface IFetchActiveEventWrapper {
+  setEvent: React.Dispatch<React.SetStateAction<IEvent>>;
+}
+
+export default function FetchActiveEventWrapper(props: IFetchActiveEventWrapper): null {
+  const setEvent = props.setEvent
   useEffect(() => {
     let mounted = true
     fetchEvent().then(data => {
       if (mounted) {
         if (data.id === '0-0') {
-          setEvent({ id: '0-0' })
+          setEvent({ id: '0-0', maxUnits: 0, start: new Date(), name: '' })
         } else {
           const utcStart = new Date(data.Start)
           const localStart = new Date(utcStart.getTime() - (utcStart.getTimezoneOffset() * 60000));
