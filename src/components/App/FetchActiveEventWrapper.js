48b77f05beb466ed088635e47a4a8fb2ce5d5870
Mike Rhoden <mike.rhoden.dev@gmail.com>
2021-10-13 16:50:41 -0500

    format document FetchEventWrapper.js

--- src/components/App/FetchActiveEventWrapper.js
+++ src/components/App/FetchActiveEventWrapper.js
@@ -2,27 +2,27 @@ import { useEffect } from 'react'
 import { fetchEvent } from '../../services/activeEvent'
 
 export default function FetchActiveEventWrapper({ setEvent }) {
-    useEffect(() => {
-        let mounted = true
-            fetchEvent().then(data => {
-                if (mounted) {     
-                  if (data.id === '0-0') {
-                    setEvent({ id: '0-0'})
-                  } else {
-                    const utcStart = new Date(data.Start)
-                    const localStart = new Date( utcStart.getTime() - (utcStart.getTimezoneOffset() * 60000));
-                    setEvent({
-                      id: data.EventId,
-                      start: localStart,
-                      maxUnits: data.MaxUnits,
-                      name: data.Name
-                    })
-                  }
-                }
-              })
-        return () => {
-            mounted = false
+  useEffect(() => {
+    let mounted = true
+    fetchEvent().then(data => {
+      if (mounted) {
+        if (data.id === '0-0') {
+          setEvent({ id: '0-0' })
+        } else {
+          const utcStart = new Date(data.Start)
+          const localStart = new Date(utcStart.getTime() - (utcStart.getTimezoneOffset() * 60000));
+          setEvent({
+            id: data.EventId,
+            start: localStart,
+            maxUnits: data.MaxUnits,
+            name: data.Name
+          })
         }
-      }, [setEvent])
-    return null;
+      }
+    })
+    return () => {
+      mounted = false
+    }
+  }, [setEvent])
+  return null;
 }
